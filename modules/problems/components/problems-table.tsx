e2d7cef9883fb2774e4c3c3d487bac377c4fd668
Happy-negi238 <happynegi238@gmail.com>
2026-08-03 13:28:18 +0530

    fetch all problems and show on ui

---
+++ modules/problems/components/problems-table.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { useProblemFilters } from "../hooks/use-problem-filters";
+import { ProblemsFilters } from "./problem-filters";
+import { ProblemsHeader } from "./problems-header";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { usePagination } from "../hooks/use-pagination";
+import { ProblemRow } from "./problem-row";
+import { ProblemsEmpty } from "./problem-empty";
+import { ProblemsPagination } from "./problems-pagination";
+
+export const ProblemsTable = ({ problems = [], user }: any) => {
+  const filters = useProblemFilters(problems);
+  const pagination = usePagination(filters.filteredProblems)
+
+  return (
+    <div className="w-full max-w-7xl mx-auto space-y-8 p-6">
+      <ProblemsHeader onCreatePlaylist={() => {}} />
+      <ProblemsFilters
+        search={filters.search}
+        onSearchChange={filters.setSearch}
+        difficulty={filters.difficulty}
+        onDifficultyChange={filters.setDifficulty}
+        selectedTag={filters.selectedTag}
+        onTagChange={filters.setSelectedTag}
+        allTags={filters.allTags}
+      />
+
+      <Card>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-25">Solved</TableHead>
+                <TableHead>Title</TableHead>
+                <TableHead>Tags</TableHead>
+                <TableHead className="w-30">Difficulty</TableHead>
+                <TableHead className="w-50">Actions</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {pagination.paginatedItems.length > 0 ? (
+                pagination.paginatedItems.map((problem) => (
+                  <ProblemRow
+                    key={problem.id}
+                    problem={problem}
+                    user={user}
+                    onDelete={() => {}}
+                    onSave={() => {}}
+                  />
+                ))
+              ) : (
+                <ProblemsEmpty />
+              )}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+
+      {pagination.showPagination && (
+        <ProblemsPagination
+          currentPage={pagination.currentPage}
+          totalPages={pagination.totalPages}
+          displayRange={pagination.displayRange}
+          canGoPrevious={pagination.canGoPrevious}
+          canGoNext={pagination.canGoNext}
+          onPrevious={pagination.goToPreviousPage}
+          onNext={pagination.goToNextPage}
+        />
+      )}
+    </div>
+  );
+};
